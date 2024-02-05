2249a79b8d4440020124edbf6b3dca6907b08548
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2024-02-05 16:36:22 +0100

    configurable host ip & port. form example with ts

--- src/frontend/src/components/CreateUserComponent.vue
+++ src/frontend/src/components/CreateUserComponent.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-  const handleSubmit = () => console.log('Submit')
+  const handleSubmit = () => console.log('Submit');
+  let username: string = '';
+  const users = [ 'Foo', 'Bar' ];
 </script>
 
 <template>
   <h2>Create User Component</h2>
-  <div>
+  <div id="create-user-section">
+    <h3>Create User</h3>
     <form @submit.prevent="handleSubmit">
-      <label>Username</label>
-      <input v-model="username" required type="text">
+      <label for="username-input">Username</label>
+      <input id="username-input" v-model="username" required type="text">
       <button type="submit">Submit</button>
     </form>
+  </div>
+  <div id="users-section">
+    <h3>Users</h3>
+    <li v-for="(item, index) in users" :key="index">
+      {{ item }}
+    </li>
   </div>
 </template>
 
