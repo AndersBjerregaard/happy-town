7ce3b51a3ae4817ffeddfb64876a9cdf072f31c0
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2024-02-08 07:41:40 +0100

    added user_endpoints.rs module

--- src/user_service/src/main.rs
+++ src/user_service/src/main.rs
@@ -1,47 +1,12 @@
-use rocket::{fairing::{Fairing, Info, Kind}, http::{Header, Status}, serde::{json::Json, Deserialize, Serialize}, Request, Response};
+pub mod user_endpoints;
+
+use rocket::{fairing::{Fairing, Info, Kind}, http::Header, Request, Response};
 
 #[macro_use] extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
-}
-// Users section
-
-#[get("/user")]
-async fn get_users() -> Json<String> {
-    todo!()
-}
-
-#[get("/user/<id>")]
-async fn get_user(id: String) -> Json<String> {
-    todo!()
-}
-
-#[options("/user")]
-fn user_options<'r>() -> Status {
-    Status::Ok
-}
-
-#[post("/user", data = "<user>")]
-async fn create_user(user: Json<User>) -> Json<String> {
-    todo!()
-}
-
-#[put("/user", data = "<user>")]
-async fn update_user(user: Json<User>) -> Json<String> {
-    todo!()
-}
-
-#[delete("/user/<id>")]
-async fn delete_user(id: String) -> Json<String> {
-    todo!()
-}
-
-#[derive(Serialize,Deserialize)]
-struct User {
-    id: String,
-    username: String,
 }
 
 pub struct CORS;
@@ -56,7 +21,7 @@ impl Fairing for CORS {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:5173"));
+        response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:8080"));
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
